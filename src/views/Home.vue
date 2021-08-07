ab6ebd86549629b18ae07caa6d293e9eb6818e4f
anis-meliti <ansmeliti@gmail.com>
2021-08-07 22:53:06 +0100

    create the blog post component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost
+      :post="post"
+      v-for="(post, index) in sampleBlogPost"
+      :key="index"
+    />
   </div>
 </template>
 
 <script>
+import BlogPost from "../Components/BlogPost.vue";
+
 export default {
   name: "Home",
-  components: {},
-  
+  components: {
+    BlogPost,
+  },
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome!",
+        blogPost:
+          '"Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post! ',
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPost: [
+        {
+          title: "this a filter Title!",
+          blogHTML: "This is a filter blog post title!",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: "this a filter Title!",
+          blogHTML: "This is a filter blog post title!",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+      ],
+    };
+  },
 };
 </script>
